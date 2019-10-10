420b1f5ea0025ab48813728a3d595b1e6a15a7e6
michelealda <michele.alda@gmail.com>
2019-10-10 15:35:16 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as R from "ramda";
+
+const Expense = props => {
+  const { month, value } = props;
+  return (
+    <div style={{ display: "block" }}>
+      ${value} {month}
+    </div>
+  );
+};
+
+const InputExpense = props => {
+  const { value, onUpdate, onClick } = props;
+
+  return (
+    <>
+      <input value={value} onChange={onUpdate} />
+      <button onClick={onClick}>Add</button>
+    </>
+  );
+};
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [money, setMoney] = useState(0);
+
+  const updateExpenseValue = e => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+    setMoney(value);
+  };
+  const addToList = () => {
+    setExpenses(R.append({ month: "Feb", money }, expenses));
+  };
+
+  const mapIndexed = R.addIndex(R.map);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <InputExpense
+        value={money}
+        onUpdate={updateExpenseValue}
+        onClick={addToList}
+      />
+      {mapIndexed(
+        (e, i) => (
+          <Expense key={i} month={e.month} value={e.money} />
+        ),
+        expenses
+      )}
+    </>
   );
 }
 
